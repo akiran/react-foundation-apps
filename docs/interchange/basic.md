1cc6ebfe7afeaff2257b8a45e8b9cc59f00ea224
kiran <kiran@webrafter.com>
2014-12-19 21:32:50 +0530

    Added docs for interchange

---
+++ docs/interchange/basic.md
@@ -0,0 +1,7 @@
+```html
+<Interchange>
+  <img media="small" src="/img/small.jpg" />
+  <img media="medium" src="/img/medium.jpg" />
+  <img media="large" src="/img/large.jpg" />
+</Interchange>
+```
--- docs/interchange/index.jsx
+++ docs/interchange/index.jsx
@@ -1,11 +1,28 @@
 var React = require('react');
 var BasicInterchange = require('./basic');
+var Highlight = require('react-highlight');
 
 var InterchangeDocs = React.createClass({
   render: function () {
     return (
       <div>
-         <BasicInterchange /> 
+        <h2>Interchange</h2>
+        <h4 className='subheader'>A simple JavaScript solution for responsive images and content. Interchange uses media queries to dynamically load responsive content that is appropriate for different users' browsers</h4>
+        <hr />
+          <p>Resize the page to see the content change.</p>
+          <BasicInterchange /> 
+        <hr/>
+        <h3>Basic</h3>
+        <div className='grid-block'>
+          <div className='grid-content'>
+            <Highlight innerHTML={true}>
+              {require('./basic.md')}
+            </Highlight>
+          </div>
+          <div className='grid-content'>
+            <BasicInterchange />
+          </div>
+        </div> 
       </div>
     );
   }
