52197f10cac728fe7db9b6ed6e91e934c60742ce
kiran <kiran@webrafter.com>
2014-12-19 21:08:30 +0530

    Fix for dynamic src of iconic

--- lib/iconic/index.jsx
+++ lib/iconic/index.jsx
@@ -1,10 +1,17 @@
 var React = require('react');
 var IconicJs = require('../vendor/iconic.min');
+var cloneWithProps = require('react/lib/cloneWithProps');
 
 var Iconic = React.createClass({
-  componentDidMount: function () {
+  inject: function () {
     var ico = IconicJs();
     ico.inject(this.getDOMNode());
+  },
+  componentDidMount: function () {
+    this.inject();
+  },
+  componentDidUpdate: function () {
+    this.inject();
   },
   render: function () {
     return React.Children.only(this.props.children);
