1294a9fa5f6648870eedcc5bccc2b2cb462ea36b
kiran <kiran@webrafter.com>
2015-10-14 22:42:17 +0530

    updated docs

--- docs/index.jsx
+++ docs/index.jsx
@@ -1,9 +1,9 @@
 var React = require('react');
 var Router = require('react-router');
 var routes = require('./routes');
-var ExecutionEnvironment = require('react/src/ExecutionEnvironment');
-
-if (ExecutionEnvironment.canUseDOM) {
+var canUseDOM = require('can-use-dom');
+ 
+if (canUseDOM) {
   document.addEventListener('DOMContentLoaded', function () {
     Router.run(routes, Router.HistoryLocation, function (Handler) {
       React.render(<Handler/>, document.body);
